b84a714f2876375a7a6f386a8574a30bbde318cb
Jaqueline Donin <40238428+Jaquedonin@users.noreply.github.com>
2024-04-20 00:38:33 -0300

    Update previsor_v2.py
    tirando salvamento

--- previsor_v2.py
+++ previsor_v2.py
@@ -48,9 +48,9 @@ model = YOLO('yolov8n.pt')
 arquivo = open('speed3.text' , 'a')
 for i in range(300):
     source = 'images/img'+str(i+1)+'.jpg'
-    print(source)
+    # print(source)
     # Run inference on the source
-    results = model(source, save=True, conf=0.5)  # list of Results objects
+    results = model(source, conf=0.5)  # list of Results objects
     # print(results[0].speed['postprocess'] + results[0].speed['preprocess'] + results[0].speed['inference'])
     soma = results[0].speed['postprocess'] + results[0].speed['preprocess'] + results[0].speed['inference']
     aux_inserir = str(soma)+ "\n"
