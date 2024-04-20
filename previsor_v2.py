595751cb8d5b91ffc81c50ab7bb65fb9bc5fbfc4
Jaqueline Donin <40238428+Jaquedonin@users.noreply.github.com>
2024-04-20 13:08:33 -0300

    Update previsor_v2.py
    removendo prints

--- previsor_v2.py
+++ previsor_v2.py
@@ -48,7 +48,7 @@ model = YOLO('yolov8n.pt')
 arquivo = open('speed3.text' , 'a')
 for i in range(300):
     source = 'images/img'+str(i+1)+'.jpg'
-    print(source)
+    # print(source)
     # Run inference on the source
     results = model(source, conf=0.5)  # list of Results objects
     # print(results[0].speed['postprocess'] + results[0].speed['preprocess'] + results[0].speed['inference'])
